3be928d0cd3b1687137e07ce0e5b22ce9c0e9143
Erdin Hermawan <erdinhrmwn@gmail.com>
2022-07-05 21:22:22 +0700

    Add models, migrations, seeders

--- database/migrations/2022_07_05_125840_create_teams_table.php
+++ database/migrations/2022_07_05_125840_create_teams_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('icon');
+            $table->unsignedBigInteger('company_id');
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('teams');
     }
 };
