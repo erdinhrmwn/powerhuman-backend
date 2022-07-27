0ac9170d2f848101c579263da0e1a2c44f20e92c
Erdin Hermawan <erdinhrmwn@gmail.com>
2022-07-27 11:17:15 +0700

    Add pint.json

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
     {
         $name = $this->faker->name();
         $username = $this->faker->unique()->userName();
-        $email = $username . '@powerhuman.net';
+        $email = $username.'@powerhuman.net';
         $password = bcrypt('password');
 
         return [
@@ -60,14 +60,14 @@ class UserFactory extends Factory
      */
     public function withPersonalTeam()
     {
-        if (!Features::hasTeamFeatures()) {
+        if (! Features::hasTeamFeatures()) {
             return $this->state([]);
         }
 
         return $this->has(
             Team::factory()
                 ->state(function (array $attributes, User $user) {
-                    return ['name' => $user->name . '\'s Team', 'user_id' => $user->id, 'personal_team' => true];
+                    return ['name' => $user->name.'\'s Team', 'user_id' => $user->id, 'personal_team' => true];
                 }),
             'ownedTeams'
         );
